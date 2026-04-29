740a76542a1a06589b833940ee2aa320d4e2b2d7
Abdumalik Shukrilloev <shamf120804@gmail.com>
2026-04-29 23:35:30 +0200

    Login & SignUp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+// import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Dashboard/Home";
+import Income from "./Pages/Dashboard/Income";
+import Expense from "./Pages/Dashboard/Expense";
+import Login from "./Pages/Auth/Login";
+import SignUp from "./Pages/Auth/SignUp";
 
 const App = () => {
-  return <div className="text-2xl text-purple-600">App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/income" element={<Income />} />
+        <Route path="/expense" element={<Expense />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
+
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
+ 
+  return isAuthenticated ? (
+    <Navigate to={"/dashboard"} />
+  ) : (
+    <Navigate to={"/login"} />
+  );
+};
