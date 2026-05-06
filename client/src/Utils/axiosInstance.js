ba0a5ab4121d0b9a0ad3dafd94520ae3628e6401
Abdumalik Shukrilloev <shamf120804@gmail.com>
2026-05-06 15:43:20 +0200

    Axios Setup

---
+++ client/src/Utils/axiosInstance.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { BASE_URL } from "./apiPaths";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+//Request Interceprtors
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+//Response Interceprtors
+axiosInstance.interceptors.response.user(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 401) {
+        window.location.href = "/login";
+      } else if (error.response.status === 500) {
+        console.log("Server error. PLease try again later");
+      }
+    } else if (error.code === "ECONNABORTED") {
+      console.log("Request timeout. Please try again");
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
